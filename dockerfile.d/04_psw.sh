6518060534b7b31d47b66f97db5878bb67c47403
Cyrux-Network <netcyrux@gmail.com>
2025-07-16 16:25:33 -0600

    docker: add Docker build configurations and helpers

---
+++ dockerfile.d/04_psw.sh
@@ -0,0 +1,15 @@
+apt-get update && apt-get install -y dkms gnupg2 apt-transport-https software-properties-common
+curl -fsSLo /usr/share/keyrings/intel-sgx-deb.asc https://download.01.org/intel-sgx/sgx_repo/ubuntu/intel-sgx-deb.key
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/intel-sgx-deb.asc] https://download.01.org/intel-sgx/sgx_repo/ubuntu $(lsb_release -sc) main" | tee /etc/apt/sources.list.d/intel-sgx.list
+apt-get update && \
+apt-get install -y \
+    libsgx-aesm-launch-plugin \
+    libsgx-enclave-common \
+    libsgx-epid \
+    libsgx-launch \
+    libsgx-quote-ex \
+    libsgx-uae-service \
+    libsgx-urts \
+    libsgx-dcap-quote-verify && \
+mkdir /var/run/aesmd && \
+rm -rf /var/lib/apt/lists/*
